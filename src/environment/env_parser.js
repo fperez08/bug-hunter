9785ee3eb62ad20ca6f287956c1226d031a24951
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-07 10:42:21 -0600

    New function to get a json from a given path

--- src/environment/env_parser.js
+++ src/environment/env_parser.js
@@ -1,3 +1,7 @@
+exports.getJsonFromFile = function (path) {
+    return require(`${process.cwd()}${path}`);
+};
+
 exports.env = function (key) {
     const value = process.env[key];
     if (!value) throw Error(`${key} value not found in the environment`);

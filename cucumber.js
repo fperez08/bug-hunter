6d1549c9d3c806b7c788e0ea678da4e0c98d31bc
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-07 10:41:01 -0600

    Add world parameters

--- cucumber.js
+++ cucumber.js
@@ -1,12 +1,19 @@
 const dotenv = require("dotenv");
-const { env } = require("$/src/environment/env_parser.js");
+const { env, getJsonFromFile } = require("$/src/environment/env_parser.js");
 
 dotenv.config({ path: env("COMMON_CONFIG_FILE") });
+
+const hostsConfig = getJsonFromFile(env("HOSTS_URLS_PATHS"));
+const pagesConfig = getJsonFromFile(env("PAGE_URLS_PATHS"));
 
 const common = {
     paths: ["./src/features/**/*.feature"],
     require: ["./src/step-definitions/**/**/*.js"],
     format: ["progress-bar", "json:./reports/report.json"],
+    worldParameters: {
+        hostsConfig: { ...hostsConfig },
+        pagesConfig: { ...pagesConfig },
+    },
 };
 
 module.exports = {

30170175c474c53129ae9cdd362b3564993e0de9
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-07 13:04:46 -0600

    Read and pass element mappings

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,25 @@
 const dotenv = require("dotenv");
 const { env, getJsonFromFile } = require("$/src/environment/env_parser.js");
+const fs = require("fs");
 
 dotenv.config({ path: env("COMMON_CONFIG_FILE") });
 
 const hostsConfig = getJsonFromFile(env("HOSTS_URLS_PATHS"));
 const pagesConfig = getJsonFromFile(env("PAGE_URLS_PATHS"));
+const mappingFiles = fs.readdirSync(
+    `${process.cwd()}${env("PAGE_ELEMENTS_PATH")}`
+);
+
+const pageElementsMappings = mappingFiles.reduce(
+    (pageElementConfigAcc, file) => {
+        const key = file.replace(".json", "");
+        const elementMappings = getJsonFromFile(
+            `${env("PAGE_ELEMENTS_PATH")}${file}`
+        );
+        return { ...pageElementConfigAcc, [key]: elementMappings };
+    },
+    {}
+);
 
 const common = {
     paths: ["./src/features/**/*.feature"],
@@ -13,6 +28,7 @@ const common = {
     worldParameters: {
         hostsConfig: { ...hostsConfig },
         pagesConfig: { ...pagesConfig },
+        pageElementsMappings: { ...pageElementsMappings },
     },
 };
 

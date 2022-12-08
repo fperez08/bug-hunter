cf2a45d400ef05bb843980ff7d2f9418b723de94
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-07 19:49:27 -0600

    chai wrapper function to retry assertions

---
+++ src/support/chai_retry_assertions.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+const { retryWithDelay } = require("$/src/support/promise_retry.js");
+
+exports.expect = function (actual) {
+    const defaultOptions = { retry: 50, waitTime: 100 };
+    this.toContain = async function (expected, options = defaultOptions) {
+        const { retry, waitTime } = options;
+        await retryWithDelay(
+            async () => {
+                expect(await actual).to.contain(expected);
+            },
+            retry,
+            waitTime
+        );
+    };
+    this.toBeTrue = async function (options = defaultOptions) {
+        const { retry, waitTime } = options;
+        await retryWithDelay(
+            async () => {
+                expect(await actual).to.be.true;
+            },
+            retry,
+            waitTime
+        );
+    };
+    return this;
+};

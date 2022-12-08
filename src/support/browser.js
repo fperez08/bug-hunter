01af82f4edc9a29aab05a53373e10f785847cb1a
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-08 10:20:53 -0600

    New function to test if we are in the correct page

--- src/support/browser.js
+++ src/support/browser.js
@@ -1,4 +1,4 @@
-exports.navigateTo = async (page, pageId, globalConfigs) => {
+const navigateTo = async (page, pageId, globalConfigs) => {
     const hostName = process.env["UI_AUTOMATION_HOST"] || "localhost";
     const { pagesConfig, hostsConfig } = globalConfigs;
 
@@ -8,3 +8,18 @@ exports.navigateTo = async (page, pageId, globalConfigs) => {
     url.pathname = pageConfigItem.route;
     await page.goto(url.href);
 };
+
+const currentPathMatchPageId = async (page, pageId, globalConfigs) => {
+    const currentUrl = await page.url();
+    const { pathname } = new URL(currentUrl);
+    return pathMatchesPageId(pathname, pageId, globalConfigs);
+};
+
+const pathMatchesPageId = (path, pageId, globalConfigs) => {
+    const { pagesConfig } = globalConfigs;
+    const pageRegex = new RegExp(pagesConfig[pageId].regex);
+    return pageRegex.test(path);
+};
+
+exports.navigateTo = navigateTo;
+exports.currentPathMatchPageId = currentPathMatchPageId;

da9da96f9263eaaf87d553cceecb8c32d2c0f1e2
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-12-08 13:01:33 -0600

    Use plw expect and update getSelector

--- src/step-definitions/assertions/verify-element-visibility.js
+++ src/step-definitions/assertions/verify-element-visibility.js
@@ -1,34 +1,13 @@
 const { Then } = require("@cucumber/cucumber");
-const { expectAsync } = require("$/src/support/chai_retry_assertions.js");
-const { getLocator } = require("$/src/support/web_element.js");
-Then(
-    /^the "([^"]*)" should contain the text "(.*)"$/,
-    async function (elementKey, expectedElementText) {
-        const {
-            screen: { page },
-            globalConfigs,
-            globalVariables,
-        } = this;
-        console.log(
-            `the ${elementKey} should contain the text ${expectedElementText}`
-        );
-
-        const selector = getLocator(elementKey, globalVariables, globalConfigs);
-        const elementText = await page.locator(selector).innerText();
-
-        await expectAsync(page.locator(selector).innerText()).toContain(
-            expectedElementText
-        );
-    }
-);
+const { expect } = require("@playwright/test");
+const { getSelector } = require("$/src/support/web_element.js");
 
 Then(/the "([^"]*)" should be displayed$/, async function (elementKey) {
     const {
         screen: { page },
         globalConfigs,
-        globalVariables,
     } = this;
 
-    const selector = getLocator(elementKey, globalVariables, globalConfigs);
-    await expectAsync(page.locator(selector).isVisible()).toBeTrue();
+    const selector = await getSelector(page, elementKey, globalConfigs);
+    await expect(page.locator(selector)).toBeVisible();
 });
